0e3ec05b930a16ac691940feccc1040e5bef19fe
Sruthik P <psruthik@gmail.com>
2018-03-22 14:28:24 +0000

    Imp changes to Octave code

--- code/blog1Plot.m
+++ code/blog1Plot.m
@@ -1,11 +1,15 @@
-data = csvread('../datasets/housingPricesMini.csv');
+#!/usr/bin/octave -qf
+data = csvread("../datasets/housingPricesMini.csv")
 area = data(:,1);
 price = data(:,2);
 xlabel("Area");
 ylabel("Price");
 title("Housing Prices - 1000 Nos");
+x = 1:5000;
 s = scatter(area, price, "filled");
 hold on
-saveas(s, "test.png");
+plot(x, 16.310*x);
+hold off
+saveas(s, "testCLI.png");
 
 
